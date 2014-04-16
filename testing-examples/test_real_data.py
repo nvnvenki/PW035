afa52e53e025b505307b5e719a8308ade34b9569
BhuvanAnand <bhuvanlive@gmail.com>
2014-04-16 21:06:45 +0530

    handling too big real data.. order of GB. uses hdf5 library for handling huge data, matrices.

--- testing-examples/test_real_data.py
+++ testing-examples/test_real_data.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import test_gmm_win
+import time
 import numpy as np
 from tempfile import mkdtemp
 import multiprocessing as mp
+import cPickle
+import h5py
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
@@ -40,7 +43,7 @@ def getChunks(fileslist, numprocs):
 
 def getMfccs(fileslist, sharedlist, lock):
     for filepath in fileslist:
-	print 'accessing file: ', filepath
+        print 'accessing file: ', filepath
 
         audio = sonusreader.SonusReader.from_file(filepath)
 
@@ -55,16 +58,18 @@ def getMfccs(fileslist, sharedlist, lock):
         # release lock
         lock.release()
 
+        print 'done processing file: ', filepath
+
 def main():
-    kannada = "C:\\Users\\bhuvan\\Desktop\\audio\\kannada"
-    english = "C:\\Users\\bhuvan\\Desktop\\audio\\english"
+    kannada = "C:\\Users\\bhuvan\\Desktop\\split_files\\kannada"
+    english = "C:\\Users\\bhuvan\\Desktop\\split_files\\english"
 
     kan_files = test_gmm_win.list_files(['--dirpath=' + kannada])
     english_files = test_gmm_win.list_files(['--dirpath=' + english])
 
     numfiles = min(len(kan_files), len(english_files))
 
-    len_train_set = int(round(0.6 * numfiles))
+    len_train_set = int(round(0.7 * numfiles))
 
     print len_train_set
 
@@ -73,10 +78,8 @@ def main():
         training_set.extend([os.path.join(kannada, kan_files[i]),
         os.path.join(english, english_files[i])])
 
-    training_set = training_set[0:6]
-
     # number of processes
-    numprocs = 3
+    numprocs = 4
 
     # create a lock for synchronization
     lock = mp.Lock()
@@ -110,18 +113,41 @@ def main():
     print 'done computing mfcc'
 
     # create a memory mapped file
-    filename = os.path.join(mkdtemp(), 'memfile')
+    # filename = os.path.join(mkdtemp(), 'memfile')
 
-    memarrray = np.memmap(filename, dtype=np.float32, mode='w+', shape=data.shape)
+    dim = data.shape
+
+    print 'dimension', dim
+
+    # pickle the data into files
+
+    parts = getChunks(data, 5)
+
+    path = "C:\\Users\\bhuvan\\Desktop\\split_files\\mfcc-data"
+
+    for i in range(len(parts)):
+        fobj = open(os.path.join(path, "PART_" + str(i)), "wb")
+        cPickle.dump(data[parts[i][0]:parts[i][1]], fobj)
+        fobj.close()
+
+    # stored mfcc data into multiple pickled files
+
+    # now store the data into a hdf5 dataset
+    hdffile = h5py.File("C:\\Users\\bhuvan\\Desktop\\split_files\\mfcc_data.hdf5", "w")
+
+    # now create a dataset
+    hdfdataset = hdffile.create_dataset("mfcc_data", shape=dim, dtype=np.float32, data=data)
+
+    # memarrray = np.memmap(filename, dtype=np.float32, mode='w+', shape=dim)
 
     print 'created memfile map'
 
-    memarrray[:] = data[:]
+    # memarrray[:] = data[:]
 
     print 'done copying to memfile'
 
-    GMM = gmm.GaussianMixtureModel(memarrray, 2, options={
-        'method':'uniform'
+    GMM = gmm.GaussianMixtureModel(hdfdataset, 2, options={
+        'method':'kmeans'
         })
 
     print 'gmm object construction done'
@@ -130,7 +156,9 @@ def main():
 
     print 'emm done'
 
-    gmm.GaussianMixtureModel.saveobject(GMM, filepath="C:\\Users\\bhuvan\\sonus\\newsdata")
+    gmm.GaussianMixtureModel.saveobject(GMM, filepath="C:\\Users\\bhuvan\\Desktop\\split_files")
 
 if __name__ == '__main__':
+    start = time.time()
     main()
+    print str((time.time() - start)/60) + " minutes"
